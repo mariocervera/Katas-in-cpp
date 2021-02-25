448ecfaeded4dae5a442d310dd4b4830da392662
Mario Cervera <mariocervera@gmail.com>
2021-02-25 02:42:53 +0100

    Prime factors - Passes acceptance test

--- PrimeFactors/src/PrimeFactorsCalculator.cpp
+++ PrimeFactors/src/PrimeFactorsCalculator.cpp
@@ -1,7 +1,11 @@
 #include "PrimeFactorsCalculator.hpp"
 
 vector<unsigned> PrimeFactorsCalculator::getPrimeFactors(unsigned num) {
-	vector<unsigned> factors;
+  vector<unsigned> factors;
 
-	return factors;
+  for (int divisor = 2; num > 1; ++divisor)
+    for (; num % divisor == 0; num /= divisor)
+      factors.emplace_back(divisor);
+
+  return factors;
 }
--- PrimeFactors/tests/PrimeFactorsTests.cpp
+++ PrimeFactors/tests/PrimeFactorsTests.cpp
@@ -13,7 +13,7 @@ protected:
 
   // Custom assertion
   void assertFactors(unsigned num, const vector<unsigned> &factors) {
-      ASSERT_TRUE(calculator->getPrimeFactors(num) == factors);
+    ASSERT_TRUE(calculator->getPrimeFactors(num) == factors);
   }
 
   std::unique_ptr<PrimeFactorsCalculator> calculator;
@@ -22,7 +22,35 @@ protected:
 ///
 /// Tests
 /// 
-TEST_F(PrimeFactorsCalculatorFixture, 0_and_1_shouldReturnEmptyList) {
-    assertFactors(0, {});
-    assertFactors(1, {});
+TEST_F(PrimeFactorsCalculatorFixture, 0_and_1_shouldReturn_None) {
+  assertFactors(0, {});
+  assertFactors(1, {});
+}
+
+TEST_F(PrimeFactorsCalculatorFixture, 2_shouldReturn_2) {
+  assertFactors(2, {2});
+}
+
+TEST_F(PrimeFactorsCalculatorFixture, 3_shouldReturn_3) {
+  assertFactors(3, {3});
+}
+
+TEST_F(PrimeFactorsCalculatorFixture, 4_shouldReturn_2_2) {
+  assertFactors(4, {2, 2});
+}
+
+TEST_F(PrimeFactorsCalculatorFixture, 6_shouldReturn_2_3) {
+  assertFactors(6, { 2, 3 });
+}
+
+TEST_F(PrimeFactorsCalculatorFixture, 8_shouldReturn_2_2_2) {
+  assertFactors(8, { 2, 2, 2 });
+}
+
+TEST_F(PrimeFactorsCalculatorFixture, 9_shouldReturn_3_3) {
+  assertFactors(9, { 3, 3 });
+}
+
+TEST_F(PrimeFactorsCalculatorFixture, AcceptanceTest) {
+  assertFactors(2*2*3*5*5*7*7*11, { 2, 2, 3, 5, 5, 7, 7, 11 });
 }
