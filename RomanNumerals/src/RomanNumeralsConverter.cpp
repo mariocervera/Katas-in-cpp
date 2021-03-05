8fb9acb00e66422a39c569d19c63e39fb0aab956
Mario Cervera <mariocervera@gmail.com>
2021-03-06 00:52:01 +0100

    Remove tabs

--- RomanNumerals/src/RomanNumeralsConverter.cpp
+++ RomanNumerals/src/RomanNumeralsConverter.cpp
@@ -1,36 +1,36 @@
 #include "RomanNumeralsConverter.hpp"
 
 static string unit() {
-		return "I";
+  return "I";
 }
 
 static string fifth() {
-	return "V";
+  return "V";
 }
 
 static string tenth() {
-	return "X";
+  return "X";
 }
 
 string RomanNumeralsConverter::getRomanNumeral(unsigned number) {
-	string romanNumeral = "";
+  string romanNumeral = "";
 
-	if (number <= 3)
-		for (unsigned i = 1; i <= number; ++i)
-			romanNumeral += unit();
+  if (number <= 3)
+    for (unsigned i = 1; i <= number; ++i)
+      romanNumeral += unit();
 
-	if (number == 4 || number == 9)
-		romanNumeral += unit();
+  if (number == 4 || number == 9)
+    romanNumeral += unit();
 
-	if (4 <= number && number <= 8)
-		romanNumeral += fifth();
+  if (4 <= number && number <= 8)
+    romanNumeral += fifth();
 
-	if(6 <= number && number <= 8)
-		for (unsigned i = 6; i <= number; ++i)
-			romanNumeral += unit();
+  if(6 <= number && number <= 8)
+    for (unsigned i = 6; i <= number; ++i)
+      romanNumeral += unit();
 
-	if (9 <= number)
-		romanNumeral += tenth();
+  if (9 <= number)
+    romanNumeral += tenth();
 
-	return romanNumeral;
+  return romanNumeral;
 }
