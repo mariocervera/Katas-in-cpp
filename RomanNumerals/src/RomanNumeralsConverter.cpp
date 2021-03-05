78922272db966b55a618ed117e6daf3a8d3b3b3b
Mario Cervera <mariocervera@gmail.com>
2021-03-06 00:31:25 +0100

    Tests for first ten roman numerals.

--- RomanNumerals/src/RomanNumeralsConverter.cpp
+++ RomanNumerals/src/RomanNumeralsConverter.cpp
@@ -1,5 +1,27 @@
 #include "RomanNumeralsConverter.hpp"
 
 string RomanNumeralsConverter::getRomanNumeral(unsigned number) {
-	return "I";
+	string romanNumeral = "";
+
+	if (number <= 3)
+		for (unsigned i = 1; i <= number; ++i)
+			romanNumeral += "I";
+
+	if (number == 4)
+		romanNumeral += "I";
+
+	if (4 <= number && number <= 8)
+		romanNumeral += "V";
+
+	if(6 <= number && number <=8)
+		for (unsigned i = 6; i <= number; ++i)
+			romanNumeral += "I";
+
+	if (number == 9)
+		romanNumeral += "I";
+
+	if (9 <= number)
+		romanNumeral += "X";
+
+	return romanNumeral;
 }
--- RomanNumerals/tests/RomanNumeralsConverterTests.cpp
+++ RomanNumerals/tests/RomanNumeralsConverterTests.cpp
@@ -24,6 +24,30 @@ protected:
 ///
 /// Tests
 ///
-TEST_F(RomanNumeralsConverterFixture, 1_shouldReturnI) {
+TEST_F(RomanNumeralsConverterFixture, 1_2_3_shouldReturnCorrectRomanNumeral) {
   assertRomanNumeral(1, "I");
+  assertRomanNumeral(2, "II");
+  assertRomanNumeral(3, "III");
+}
+
+TEST_F(RomanNumeralsConverterFixture, 4_shouldReturnCorrectRomanNumeral) {
+  assertRomanNumeral(4, "IV");
+}
+
+TEST_F(RomanNumeralsConverterFixture, 5_shouldReturnCorrectRomanNumeral) {
+  assertRomanNumeral(5, "V");
+}
+
+TEST_F(RomanNumeralsConverterFixture, 6_shouldReturnCorrectRomanNumeral) {
+  assertRomanNumeral(6, "VI");
+  assertRomanNumeral(7, "VII");
+  assertRomanNumeral(8, "VIII");
+}
+
+TEST_F(RomanNumeralsConverterFixture, 9_shouldReturnCorrectRomanNumeral) {
+  assertRomanNumeral(9, "IX");
+}
+
+TEST_F(RomanNumeralsConverterFixture, 10_shouldReturnCorrectRomanNumeral) {
+  assertRomanNumeral(10, "X");
 }
