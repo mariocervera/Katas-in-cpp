d483c06a5f041b61f6c2a088bcedf53a5db0c173
Mario Cervera <mariocervera@gmail.com>
2021-03-06 00:49:00 +0100

    Refactoring

--- RomanNumerals/src/RomanNumeralsConverter.cpp
+++ RomanNumerals/src/RomanNumeralsConverter.cpp
@@ -1,27 +1,36 @@
 #include "RomanNumeralsConverter.hpp"
 
+static string unit() {
+		return "I";
+}
+
+static string fifth() {
+	return "V";
+}
+
+static string tenth() {
+	return "X";
+}
+
 string RomanNumeralsConverter::getRomanNumeral(unsigned number) {
 	string romanNumeral = "";
 
 	if (number <= 3)
 		for (unsigned i = 1; i <= number; ++i)
-			romanNumeral += "I";
+			romanNumeral += unit();
 
-	if (number == 4)
-		romanNumeral += "I";
+	if (number == 4 || number == 9)
+		romanNumeral += unit();
 
 	if (4 <= number && number <= 8)
-		romanNumeral += "V";
+		romanNumeral += fifth();
 
-	if(6 <= number && number <=8)
+	if(6 <= number && number <= 8)
 		for (unsigned i = 6; i <= number; ++i)
-			romanNumeral += "I";
-
-	if (number == 9)
-		romanNumeral += "I";
+			romanNumeral += unit();
 
 	if (9 <= number)
-		romanNumeral += "X";
+		romanNumeral += tenth();
 
 	return romanNumeral;
 }
